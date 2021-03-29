1ef16927d6edefef4c5842b4723401d8a6307594
Sanskrita Saha <sanskrita2001@gmail.com>
2021-03-29 15:17:46 +0530

    Initial Commit

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './Styles';
+
+const Form = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            FORM
+        </div>
+    )
+}
+
+export default Form;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL =
+	'mongodb+srv://Sanskrita:Sanskrita2001@cluster0.nx8ao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify:false
+	})
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
+	)
+	.catch((error) => console.log(`${error} did not connect`));
+
