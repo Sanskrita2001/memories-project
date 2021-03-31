90ef01c2a4e3318bbce79272558b2807f6e80d26
Sanskrita Saha <sanskrita2001@gmail.com>
2021-04-01 00:14:14 +0530

    Environment Variables

--- server/index.js
+++ server/index.js
@@ -2,21 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-	'mongodb+srv://Sanskrita:Sanskrita2001@cluster0.nx8ao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL =
+// 	'mongodb+srv://Sanskrita:Sanskrita2001@cluster0.nx8ao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(CONNECTION_URL, {
+	.connect(process.env.CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify:false
