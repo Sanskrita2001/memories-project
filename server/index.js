4b23d424ad5b0d5d0e412f8528e35d99c69c00c3
Sanskrita Saha <sanskrita2001@gmail.com>
2021-04-05 18:21:38 +0530

    JWT AUTH BACKEND

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import postRoutes from './routes/posts.js';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
-// app.use(express.urlencoded());
+//app.use(express.urlencoded());
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
-// const CONNECTION_URL =
-// 	'mongodb+srv://Sanskrita:Sanskrita2001@cluster0.nx8ao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
